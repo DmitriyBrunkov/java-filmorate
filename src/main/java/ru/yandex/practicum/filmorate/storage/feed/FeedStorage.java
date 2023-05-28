9cad20b2727d80d41d4a2711edca16fe3dc1f8fc
Alex <MacMorphling@yandex.ru>
2023-05-28 23:47:47 +0300

    style: УДАЛЁН ЕЩЁ ОДИН ЛИШНИЙ ИМПОРТ

--- src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.feed;
 
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.Feed;
 
 import java.util.List;
